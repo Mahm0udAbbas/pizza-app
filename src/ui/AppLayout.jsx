68834611be4ce3ff58a772be1b3240b0b5dbc027
Mahmoud Abbas <mahmoudabbas393@gmail.com>
2024-05-13 22:26:12 +0300

    fearching menu data using loader and create loader indicator

--- src/ui/AppLayout.jsx
+++ src/ui/AppLayout.jsx
@@ -1,10 +1,14 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigation } from "react-router-dom";
 import CartOverview from "../features/cart/CartOverview";
 import Header from "./Header";
+import Loader from "./Loader";
 
 export default function AppLayout() {
+  const navidation = useNavigation();
+  const isLoading = navidation.state === "loading";
   return (
-    <div>
+    <div className="layout">
+      {isLoading && <Loader />}
       <Header />
       <main>
         <h1>Content</h1>
